4cefae8fec944a5173e777bcc1ce9d4ff000a2dd
karatirakhi374-ai <karatirakhi374@gmail.com>
2026-09-03 09:42:33 +0530

    push

---
+++ sum.java
@@ -0,0 +1,12 @@
+class ABC
+{
+    public static void main(String args[])
+    {
+        int x=50;
+        int y=20;
+        int sum=(x+y);
+        int difference=(x-y);
+            System.out.println("Sum:"+sum);
+            sytem.out.println("Difference:"+difference);
+    }
+}
